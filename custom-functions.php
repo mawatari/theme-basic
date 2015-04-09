926e3d3ea09745ade4edf02f5b710a995bb9be33
Naoto Mawatari <github.mawatari@gmail.com>
2015-04-09 09:27:10 +0900

    フロントページと検索結果の表示件数を5件に制限した

--- custom-functions.php
+++ custom-functions.php
@@ -1,1 +1,12 @@
 <?php
+// フロントページと検索結果の表示件数を5件に制限する
+add_filter('pre_get_posts', function($query) {
+	if (is_admin())
+		return;
+
+	if (is_home() || is_front_page() || is_search()) {
+		if (!$query->query_vars['posts_per_page'])
+			$query->set('posts_per_page', 5);
+	}
+});
+?>
