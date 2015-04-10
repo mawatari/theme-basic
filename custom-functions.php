6a516889b0de9687244f6aa40a5c213f6036e576
Naoto Mawatari <github.mawatari@gmail.com>
2015-04-10 12:52:26 +0900

    タイトルの後に関心度数を追加するフックを作成した

--- custom-functions.php
+++ custom-functions.php
@@ -10,3 +10,38 @@ add_filter('pre_get_posts', function($query) {
 	}
 });
 ?>
+
+<?php
+// タイトルの後に関心度数を表示する
+add_filter('the_title', function($title) {
+	// 管理画面である場合はreturn
+	if (is_admin())
+		return $title;
+
+	// メニュー等である場合はreturn
+	global $post;
+	if ($title != $post->post_title)
+		return $title;
+
+	// プラグインが有効でない場合はreturn
+	if (!function_exists('scc_get_share_total'))
+		return $title;
+
+	// カウントが0である場合はreturn
+	$count = intval(scc_get_share_total());
+	if (!$count)
+		return $title;
+
+	// タイトルに関心度数を表示する
+	$interestCount = $count == 1 ? '1 interest' : $count . ' interests';
+	if ($count <= 3) {
+		$interestClass = 'interest interest-low';
+	} elseif ($count <= 10) {
+		$interestClass = 'interest interest-middle';
+	} else {
+		$interestClass = 'interest interest-high';
+	}
+
+	return  $title . '<span class="' . $interestClass . '">' . $interestCount . '</span>';
+});
+?>
